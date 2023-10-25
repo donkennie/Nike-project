be4a28d83149e1272e3d35c9c27f277105a68222
donkennie <ajeigbekehinde160@gmail.com>
2023-10-25 17:26:57 +0100

    add navigation section

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Footer from './Footer';
+import CustomerReviews from './customerReviews';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import Subscribe from './Subscribe';
+import SuperQuality from './SuperQuality';
+
+export{
+    Hero,
+    PopularProducts,
+    CustomerReviews,
+    Footer,
+    Services,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality
+}
